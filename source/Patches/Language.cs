ee4e02493db81ba00e29b906ebc815584d2cf018
Paweł Benetkiewicz <git@benio.me>
2021-07-12 12:36:34 +0200

    add polish support

---
+++ source/Patches/Language.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TownOfUs
+{
+	public class Language
+	{
+		public string Name;
+		public Language(string name) => Name = name;
+
+		public static Language English = new Language(nameof(English));
+		public static Language Polish = new Language(nameof(Polish));
+
+		public static readonly Language Default = English;
+		public static Language Current = Default;
+
+		public static List<Language> List = new List<Language>{ English, Polish };
+		public static readonly string Location = Path.Combine
+		(
+			Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+			@"AppData\LocalLow\Innersloth\Among Us\language"
+		);
+
+		public static Language Load()
+		{
+			if (!File.Exists(Location)) {
+				Save(Default);
+				goto spare;
+			}
+
+			var languageText = File.ReadAllText(Location);
+			foreach (var language in List) {
+				if (language.Name == languageText)
+					return language;
+			}
+
+		spare:
+			return Default;
+		}
+
+		public static void Set(Language language)
+		{
+			Current = language;
+			Save(language);
+		}
+
+		public static void Save(Language language)
+		{
+			if (!File.Exists(Location))
+				File.Create(Location).Dispose();
+
+			File.WriteAllText(Location, language.Name);
+		}
+
+		public enum Dictionary
+		{
+
+		}
+	}
+
+	public static partial class LanguageExtensions
+	{
+		public static string Translate(this Language.Dictionary key) => key switch {
+			_ => key.ToString(),
+		};
+	}
+}
